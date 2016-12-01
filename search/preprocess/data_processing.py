d7ce4a430056bb0fc25547b6ef966e9297e78f6a
belgrades <fcremaldc@gmail.com>
2016-12-01 03:25:11 -0800

    Import fixed

--- search/preprocess/data_processing.py
+++ search/preprocess/data_processing.py
@@ -1,21 +1,20 @@
 import pandas as pd
-import nltk
+from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 import string
 import re
 import sys
 
-# TODO: Fix import
 
 def tokenize(text):
-    '''
+    """
     Receives a text, which could be a single word or a sentence, and applies the natural language
     process (using NLTK library) in the text, normalizing, tokenizing, removing stopwords and punctuations,
     stemming and ignoring the duplicated tokens.
     :param text: it is a string, which could be a word or a sentence
     :return: a list containing the tokens
-    '''
+    """
 
     if pd.isnull(text) or isinstance(text, bool):
         return text
@@ -30,7 +29,7 @@ def tokenize(text):
     text = re.sub(r"/", ' ', text)
 
     # creates the tokens
-    text = nltk.word_tokenize(text)
+    text = word_tokenize(text)
 
     # excludes tokens which contain numbers and punctuations
     text = [t for t in text if bool(re.search(r'\d', t)) == False and t not in string.punctuation]
@@ -50,14 +49,14 @@ def tokenize(text):
 
 def tokenize_CSV(filename='data/data.tsv',
                  columns=["recipe_name", "author", "programme", "method", "ingredients", "vegetarian"]):
-    '''
+    """
     It receives a CSV file in FILENAME containing the recipes in each row and tab-separated. It applies the
     function 'tokenize' in each cell of COLUMNS of the CSV file. It returns a Pandas dataframe.
     :param filename: TSV file containing the recipes. If nothing is specified, them it will try to open 'data.tsv' in
      the current directory
     :param columns: It is a list containing the column names of FILENAME where the function 'tokenize' will be applied
     :return: a Pandas dataframe where each row is a recipe containing the tokens of each column of COLUMNS
-    '''
+    """
     names = ["recipe_name", "author", "programme", "prep_time", "cooking_time", "serves", "pic_url",
               "method", "ingredients", "vegetarian", "calories", "protein", "carbs", "sugars",
               "total_fat", "saturated_fat", "fiber", "salt", "link"]
@@ -80,7 +79,7 @@ def tokenize_CSV(filename='data/data.tsv',
 
 
 def generate_json(df_recipes):
-    '''
+    """
     Receives a Pandas dataframe with the recipes in each row and creates a file tokens.json in the current directory
     with the recipes separated by row. The JSON file has a list-like structure indexed by the row number.
     Ex:
@@ -96,5 +95,5 @@ def generate_json(df_recipes):
     }
     :param df_recipes: Pandas dataframe with one recipe in each row
     :return:
-    '''
+    """
     df_recipes.to_json("data/tokens.json", orient="index", force_ascii=False)
