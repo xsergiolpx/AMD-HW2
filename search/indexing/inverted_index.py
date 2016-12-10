ed90bca90aaf94c384894884181835306727ae8b
belgrades <fcremaldc@gmail.com>
2016-12-10 07:16:11 -0800

    Adding cosine score measures and term frequencies

--- search/indexing/inverted_index.py
+++ search/indexing/inverted_index.py
@@ -3,9 +3,10 @@ from collections import defaultdict, Counter
 from search.download.manageFiles import save_to_tsv, read_json, write_json, read_from_tsv
 from collections import deque, Counter
 from itertools import dropwhile
+from math import sqrt, log10
+
 # TODO: Add fancy print
 # TODO: Change filename to: tokens_file, dictionary_file
-
 
 class InvertedIndex(object):
 
@@ -20,12 +21,15 @@ class InvertedIndex(object):
         self.postings = defaultdict(list)
         self.types = types
         self.dictionary = {}
+        self.term_frequencies = {}
+        self.length = {}
 
     def obtain_tokens(self):
         self.json = read_json(self.filename)
 
         if self.debug:
             n = len(self.json)
+            print(n)
             j=0
 
         for doc in self.json:
@@ -37,6 +41,8 @@ class InvertedIndex(object):
             for type in self.types:
                 try:
                     for i in self.json[doc][type]:
+                        if i == '':
+                            continue
                         self.tokens.append((i, int(doc)))
                 except TypeError:
                     continue
@@ -50,14 +56,32 @@ class InvertedIndex(object):
         self.tokens.sort(key=lambda x: (x[0], x[1]))
 
     def create_dictionary(self):
+
+        lengths = {}
+
         for k, v in self.tokens:
+            if k == '':
+                continue
             idx = self.dictionary.get(k)
             if idx is None:
+                lengths[k] = 1
                 self.dictionary[k] = {'n': 1,
-                                      'posting': "data/postings/" + k}
+                                      'posting': "data/postings/" + k,
+                                      'term_frequency': "data/term_frequencies/" + k }
             else:
-                self.dictionary[k]['n'] += 1
+                lengths[k] += 1
+                self.dictionary[k]["n"] += 1
             self.postings[k].append(v)
+
+        with open("data/documents.csv", 'r') as file_n:
+            n = int(file_n.readline())
+
+        for k in self.postings.keys():
+            self.postings[k].insert(0, n*1.0/lengths[k])
+
+    @staticmethod
+    def load_frequencies(filename):
+        return read_from_tsv(filename, lines=True)
 
     def store_postings(self):
         for k, v in self.postings.items():
@@ -66,8 +90,13 @@ class InvertedIndex(object):
             save_to_tsv(map(str, v), "data/postings/" + k)
 
     @staticmethod
-    def retrieve_posting(posting):
-        return read_from_tsv(posting)
+    def load_posting(posting):
+        with open(posting + ".tsv", "r") as f:
+            list_str = list(map(str, f.readline().split('\t')))
+            n_dft = float(list_str[0])
+            postings = list(map(int, list_str[1:]))
+
+            return [n_dft]+postings
 
     @staticmethod
     def intersection(p1, p2):
@@ -108,7 +137,7 @@ class InvertedIndex(object):
             except KeyError:
                 # Create names of k near strings to name
                 k_near = " ".join(k for k, v in self.k_near_string(name, k, type))
-                union_dict[name] = self.union(map(lambda x: self.retrieve_posting(self.dictionary[x]["posting"]),
+                union_dict[name] = self.union(map(lambda x: self.load_posting(self.dictionary[x]["posting"])[1:],
                                                   k_near.split()))
                 to_sort[name] = len(union_dict[name])
 
@@ -118,12 +147,12 @@ class InvertedIndex(object):
             if times == 0:
                 times += 1
                 try:
-                    answer = deque(self.retrieve_posting(self.dictionary[k]["posting"]))
+                    answer = deque(self.load_posting(self.dictionary[k]["posting"])[1:])
                 except KeyError:
                     answer = deque(union_dict[k])
             else:
                 try:
-                    answer = self.intersection(answer, deque(self.retrieve_posting(self.dictionary[k]["posting"])))
+                    answer = self.intersection(answer, deque(self.load_posting(self.dictionary[k]["posting"])[1:]))
                 except KeyError:
                     answer = self.intersection(answer, deque(union_dict[k]))
 
@@ -163,7 +192,7 @@ class InvertedIndex(object):
                 d[i][j] = min(d[i][j - 1] + 1, d[i - 1][j] + 1, d[i - 1][j - 1] + (not str1[i - 1] == str2[j - 1]))
         return d[N][M]
 
-    def k_near_string(self, string, k, type="tolerance"):
+    def k_near_string(self, string, k, type="near"):
         """
         Calculates either the k nearest neighbors to string or the neighbors with distance at most k from string.
         :param string: The string we want to find relatives.
@@ -173,6 +202,8 @@ class InvertedIndex(object):
         """
         distances = Counter()
         for doc in self.dictionary.keys():
+            if doc == '':
+                continue
             if doc[0] == string[0]:
                 distances[doc] = self.distance(string, doc)
 
@@ -181,7 +212,7 @@ class InvertedIndex(object):
             for k, c in dropwhile(lambda c: c[1] <= k, reversed(distances.most_common())):
                 del distances[k]
             return distances.most_common()
-        else:
+        elif type == "near":
             # Return k elements with the lowest score
             return distances.most_common()[:-k - 1:-1]
 
@@ -218,3 +249,52 @@ class InvertedIndex(object):
 
     def get_dictionary(self):
         return self.dictionary
+
+    def load_norm(self):
+        with open("data/length.tsv", 'r') as length_file:
+            for line in length_file:
+                docid, norm = line.split("\t")
+                self.length[int(docid)] = float(norm)
+
+    def cosine_score(self, q):
+        # Get the terms of the query
+        query = q.split()
+
+        # Get posting list given query
+        documents = self.intersection_list(query, 5)
+
+        # Initialize scores
+        scores = {}
+        for d in documents:
+            scores[d] = 0
+
+        # Spell correction
+        terms = []
+        for term in query:
+            exist = self.dictionary.get(term)
+            if exist is None:
+                union = [k for k, v in self.k_near_string(term, 5)]
+                terms += union
+            else:
+                terms += [term]
+
+        # Calculate w_t,q
+        wt_q = 1.0/sqrt(len(terms))
+
+        for term in terms:
+            tf_td = self.load_frequencies(self.dictionary[term]["term_frequency"])
+            posting_t = self.load_posting(self.dictionary[term]["posting"])
+            wft_d = log10(posting_t[0])
+            for doc in documents:
+                try:
+                    tf_idf_t_d = tf_td[doc] * wft_d
+                except KeyError:
+                    tf_idf_t_d = wft_d
+                scores[doc] += wt_q * tf_idf_t_d
+
+        self.load_norm()
+        # normalize
+        for doc in documents:
+            scores[doc] /= self.length[doc]
+
+        return reversed(sorted(scores.items(), key=lambda x: x[1]))
